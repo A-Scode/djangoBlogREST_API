ac0a268defe973e2607e549b044ac9777733a654
A-Scode <73399909+A-Scode@users.noreply.github.com>
2021-10-07 17:17:27 +0530

    Update settings.py

--- blog_tube/settings.py
+++ blog_tube/settings.py
@@ -61,7 +61,7 @@ CORS_ALLOW_CREDENTAILS = True
 CORS_ALLOW_HEADERS = ["*"]
 CORS_ORIGIN_WHITELIST = [
     os.environ['react_origin'],"http://192.168.43.81:3000", #static local ip
-    "https://blogtube.herokuapp.com/"
+    "https://blogtube.herokuapp.com/","https://a-scode.github.io/"
 ]
 
 ROOT_URLCONF = 'blog_tube.urls'
