4b18ae6f3e820cd4f4795f8abaee81414a12e0dc
shouryaraj <shouryarajsinghgoud@gmail.com>
2021-10-09 09:48:03 +0530

    deploying staticfiles

--- blog_tube/settings.py
+++ blog_tube/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['django_secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = ["*"]
 
