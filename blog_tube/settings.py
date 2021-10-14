057826509dff3d512cf21d785738699c3aab1e56
shouryaraj <shouryarajsinghgoud@gmail.com>
2021-10-14 23:40:16 +0530

    deploying

--- blog_tube/settings.py
+++ blog_tube/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['django_secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG =os.environ['DEBUG']
 
 ALLOWED_HOSTS = ["*"]
 
